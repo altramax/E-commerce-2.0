6e94a27b978265d66b5b72bdfc8bcaea4c4de30d
altramax <ajayiezekiel559@gmail.com>
2023-10-22 13:14:06 +0100

    mock test

--- src/Components/Templates/HomePageTemplate/HomePageTemplate.tsx
+++ src/Components/Templates/HomePageTemplate/HomePageTemplate.tsx
@@ -1,8 +1,17 @@
 import HomePageTemplateStyle from "./HomePageTemplateStyle";
 import HomeHeader from "../../Organism/HomePage/HomeManager";
 import Footer from "../../Organism/Footer/Footer";
+import axios from "axios";
+import { json } from "react-router-dom";
 
 const HomePageTemplate = (): JSX.Element => {
+
+const geti = async()=>{
+  let g =await axios.get("http://localhost:9000/products",{ responseType: "json"})
+  console.log(g);
+}
+geti()
+  
   return (
     <HomePageTemplateStyle>
       <HomeHeader />
