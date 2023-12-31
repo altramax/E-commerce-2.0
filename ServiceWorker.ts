7094a1ad9d79a12309c108e7a37bb3a40e93b59b
altramax <ajayiezekiel559@gmail.com>
2023-12-31 15:55:26 +0100

    ServieWorker File

---
+++ ServiceWorker.ts
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'my-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  // Add other assets and paths you want to cache
+];
+
+self.addEventListener('install', (event: any) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event: any) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
@@ -7,7 +6,16 @@ import { Provider } from "react-redux";
 import { persistor, store } from "../src/Redux/ConfigStore";
 import { PersistGate } from "redux-persist/integration/react";
 
-
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/ServiceWorker.ts")
+    .then((registration) => {
+      console.log("Service Worker registered with scope:", registration.scope);
+    })
+    .catch((error) => {
+      console.error("Error registering Service Worker:", error);
+    });
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
